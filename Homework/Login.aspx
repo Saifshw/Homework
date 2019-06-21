1967ad01af252b6127806ea533e6f20e4cb50d0d
PencilAmazing <saifshw@live.com>
2019-06-21 21:49:58 +0300

    Finished Login

---
+++ Homework/Login.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+	<style>
+		.LoginHolder {
+			margin-left: auto;
+			margin-right: auto;
+		}
+	</style>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div class="LoginHolder">
+		<h1>Login</h1>
+		<asp:TextBox ID="txtUser" placeholder="Username" runat="server"></asp:TextBox>
+		<p>
+			<asp:TextBox ID="txtPass" placeholder="Password" runat="server"></asp:TextBox>	
+		</p>
+		<p>
+			<asp:Button ID="Button1" runat="server" Text="Login" OnClick="Button1_Click" />
+		</p>
+		<asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Please fill in all fields" ControlToValidate="txtPass"></asp:RequiredFieldValidator>
+		<br />
+		<asp:Label ID="ErrorLabel" runat="server" Visible="False"></asp:Label>
+		</div>
+
+	</form>
+</body>
+</html>
---
+++ Homework/Login.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+public partial class Login : System.Web.UI.Page
+{
+	string ConnectionString = WebConfigurationManager.ConnectionStrings["dbConnection"].ConnectionString;
+	string SelectUserCommand = @"SELECT * FROM Login WHERE Username = @Username";
+
+	protected void Page_Load(object sender, EventArgs e)
+	{
+
+	}
+
+	protected void Button1_Click(object sender, EventArgs e)
+	{
+		RequiredFieldValidator1.Validate();
+
+		string Username = txtUser.Text;
+		string Password = txtPass.Text;
+
+		CheckInput(Username, Password);
+	}
+
+	protected void CheckInput(string User, string Pass)
+	{
+		ErrorLabel.Visible = false;
+
+		SqlConnection dbConnection = new SqlConnection(ConnectionString);
+		SqlCommand SelectUser = new SqlCommand(SelectUserCommand, dbConnection);
+		SqlDataReader Reader;
+
+		SelectUser.Parameters.Add(new SqlParameter("@Username", User));
+		try {
+			dbConnection.Open();
+			Reader = SelectUser.ExecuteReader();
+			Reader.Read();
+
+			if (!Reader.HasRows) {
+				ShowError("Username Not found");
+				return;
+			};
+
+			// Pass and user are right
+			if (Reader["Password"].ToString() == Pass) {
+
+			} else { // User right but pass isnt
+				ShowError("Welcome " + User +", your password is incorrect");
+				return;
+			}
+
+				Reader.Close();
+		}
+		catch (Exception ex) {
+
+			
+		}
+		finally {
+			dbConnection.Close();
+		}
+	}
+
+	protected void ShowError(string ErrorMessage)
+	{
+		ErrorLabel.Text = ErrorMessage;
+		ErrorLabel.Visible = true;
+	}
+
+	protected void SetElementsVisibility(bool IsVisible) {
+
+	}
+}
